6991709eb233c7eff276b188cde838b7de2269f3
edwin irungu <irungundigirigi@gmail.com>
2024-11-29 15:49:02 +0300

    populated populate right matrix

--- largest_plus_sign.py
+++ largest_plus_sign.py
@@ -17,15 +17,29 @@ def get_largest_plus(m):
     top = [[0 for _ in range(size)] for _ in range(size)]
 
     for i in range(size):
-        left[i][0] = mat[i][0]
+        left[i][0] = m[i][0]
+        right[i][size - 1] = m[i][size - 1]
+        top[0][i] = m[0][i]
+        bottom[size -1][i] = m[size -1][i]
 
     """ populate the matrixes"""
     for i in range(size):
         for j in range(1,size):
             if m[i][j] == 1:
                 left[i][j] = left[i][j - 1] + 1
+
+            if m[i][size - j] == 1:
+                right[i][size - j -1] = right[i][size - j] + 1
+
+
+    print('left')
     for i in left:
         print(i)
+
+    print('right')
+    for i in right:
+        print(i)
+
 
 if __name__=="__main__":
      
@@ -40,7 +54,6 @@ if __name__=="__main__":
             [ 1, 0, 1, 1, 1, 1, 0, 0, 1, 1 ],
             [ 1, 1, 0, 0, 1, 0, 1, 0, 0, 1 ],
             [ 1, 0, 1, 1, 1, 1, 0, 1, 0, 0 ]]
-    print('left matrix')
     print(get_largest_plus(mat))
 
 
